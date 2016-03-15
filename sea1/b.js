248b0d47f96c979390e8ba15ac91e7d9c1838032
WakeUpPig <1035465284@qq.com>
2016-03-15 20:42:26 +0800

    ''

--- sea1/b.js
+++ sea1/b.js
@@ -1,8 +1,8 @@
-//function Dialog(){
-//    alert(2)
-//}
-var obj2={
-    Dialog: function () {
+define(function (require,exports,module) {
+    //依赖需要  需要哪个文件 我们就把哪个文件引进来
+    function Dialog(){
         alert(2)
     }
-}
+    //把dialog模块进行导出
+    exports.Dialog=Dialog;
+})

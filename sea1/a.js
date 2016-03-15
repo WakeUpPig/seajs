cc08b49539de1a7eb4d54765954518bd0103a245
WakeUpPig <1035465284@qq.com>
2016-03-15 20:21:43 +0800

    'f'

---
+++ sea1/a.js
@@ -0,0 +1,32 @@
+//function Dialog(){
+//    alert(1)
+//}
+//定义一个模块
+//define(function () {
+//
+//})
+//定义一个字符串的模块
+//define('hello');
+
+
+//直接返回一个对象模块
+//独立的模块可以直接导出
+//define({
+//    Dialog: function () {
+//        alert(1)
+//    }
+//})
+
+define(function (require,exports,module) {
+    //这三个参数是不可以更改的
+    
+
+
+})
+
+
+//var obj1={
+//    Dialog: function () {
+//        alert(1)
+//    }
+//}
---
+++ sea1/b.js
@@ -0,0 +1,8 @@
+//function Dialog(){
+//    alert(2)
+//}
+var obj2={
+    Dialog: function () {
+        alert(2)
+    }
+}
